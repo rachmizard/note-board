050f97875d21ab6bd023d0cf3c7aa9405acade22
Ismail Rohaga <ismailrohaga@Ismails-MacBook-Pro.local>
2025-04-04 22:42:25 +0700

    fix(layout): remove unused import

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,4 @@
 import { AppSidebar } from "@/app/dashboard/components/app-sidebar";
-import { ModeToggle } from "@/shared/components/mode-toggle";
 import { SidebarProvider } from "@/shared/components/ui/sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
