f27779c86b35d9a2067195843a52aa71c2ac8fea
Rachmizard <rachmizard11072000@gmail.com>
2025-04-06 04:17:29 +0700

    Refactor middleware to streamline public route handling
    - Removed the `createRouteMatcher` and simplified the `clerkMiddleware` implementation to default behavior.
    - Updated the configuration to ensure webhooks remain publicly accessible while maintaining existing authentication for other routes.
    - Ensured existing functionality remains intact while enhancing the middleware structure.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,6 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/api/webhooks(.*)"]);
-
-export default clerkMiddleware(async (auth, req) => {
-  if (isPublicRoute(req)) return NextResponse.next();
-  await auth.protect();
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
@@ -14,5 +8,7 @@ export const config = {
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
     "/(api|trpc)(.*)",
+    // Webhooks are public
+    "/api/webhooks(.*)",
   ],
 };
