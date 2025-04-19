956dd0d201b0d0dca35b0dec45266ab6ca08b5fe
Rachmizard <rachmizard11072000@gmail.com>
2025-04-19 15:55:06 +0700

    feat(database): add board schema and relations
    - Introduced a new `boards` table schema with fields for `id`, `boardParentId`, `boardType`, `properties`, `content`, and `userId`.
    - Established relationships for the `boards` table, including parent-child relationships and user associations.
    - Updated migration files to create the `boards` table and its constraints.
    - Exported the new schema and relations in the index file for accessibility.

--- src/server/database/drizzle/index.ts
+++ src/server/database/drizzle/index.ts
@@ -1,7 +1,9 @@
+export * from "./board.schema";
 export * from "./tag.schema";
 export * from "./todo.schema";
 export * from "./user.schema";
 
+export * from "./board.relations";
 export * from "./tag.relations";
 export * from "./todo.relations";
 export * from "./user.relations";
